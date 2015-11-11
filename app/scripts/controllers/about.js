d31a2c5b5d472a1075cae887d575d8e3dfa31192
mlabouardy <mohamed.labouardy@gmail.com>
2015-11-11 22:06:17 +0100

    initial app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jenkinsMonitorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jenkinsMonitorApp
+ */
+angular.module('jenkinsMonitorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
